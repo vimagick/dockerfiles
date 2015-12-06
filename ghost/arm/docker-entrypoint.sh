f3ec6fd91099a77476e3fbd0224b766a3f55e5c7
kev <vimagick@gmail.com>
2015-12-06 17:21:51 +0800

    fix

--- ghost/arm/docker-entrypoint.sh
+++ ghost/arm/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [[ "$*" == npm*start* ]]; then
 		targetDir="$GHOST_CONTENT/$(basename "$dir")"
 		mkdir -p "$targetDir"
 		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
+			tar cC "$dir" . | tar xC "$targetDir"
 		fi
 	done
 
