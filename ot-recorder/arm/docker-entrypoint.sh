1bf80b6ec33a834a5a644f89a85f7c6f02f40112
kev <vimagick@gmail.com>
2017-03-31 16:43:35 +0800

    fix ot-recorder

--- ot-recorder/arm/docker-entrypoint.sh
+++ ot-recorder/arm/docker-entrypoint.sh
@@ -5,10 +5,9 @@ set -xe
 if ! [ -d $OTR_STORAGEDIR/last ]
 then
     echo "initializing ..."
-    ot-recorder --initialize
     mkdir -p $OTR_STORAGEDIR/last
+    chown -R owntracks:owntracks $OTR_STORAGEDIR
+    ot-recorder --initialize
 fi
-
-chown -R owntracks:owntracks $OTR_STORAGEDIR
 
 exec ot-recorder --http-host 0.0.0.0 $OTR_TOPICS
