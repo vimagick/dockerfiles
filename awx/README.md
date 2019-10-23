3cdd468e023c91adc43d1c30160dc6912d982461
kev <vimagick@gmail.com>
2019-10-23 18:29:57 +0800

    update awx

---
+++ awx/README.md
@@ -0,0 +1,9 @@
+awx
+===
+
+[AWX][1] provides a web-based user interface, REST API, and task engine built
+on top of Ansible. It is the upstream project for [Tower][2], a commercial
+derivative of AWX.
+
+[1]: https://github.com/ansible/awx
+[2]: https://www.ansible.com/tower
---
+++ awx/data/etc/credentials.py
@@ -0,0 +1,29 @@
+DATABASES = {
+    'default': {
+        'ATOMIC_REQUESTS': True,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "awx",
+        'USER': "awx",
+        'PASSWORD': "awxpass",
+        'HOST': "postgres",
+        'PORT': "5432",
+    }
+}
+
+BROKER_URL = 'amqp://guest:guest@rabbitmq:5672/awx'
+
+CHANNEL_LAYERS = {
+    'default': {'BACKEND': 'asgi_amqp.AMQPChannelLayer',
+                'ROUTING': 'awx.main.routing.channel_routing',
+                'CONFIG': {'url': BROKER_URL}}
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'memcached:11211'
+    },
+    'ephemeral': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
+}
