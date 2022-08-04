08ea679644b879f22f2d1e597b80dd35ec06ebbd
kev <vimagick@gmail.com>
2022-08-04 12:16:52 +0800

    update

--- node-media-server/arm/app.js
+++ node-media-server/arm/app.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const NodeMediaServer = require('node-media-server');
 
 const config = {
@@ -6,16 +8,17 @@ const config = {
     chunk_size: 60000,
     gop_cache: true,
     ping: 30,
-    ping_timeout: 60
+    ping_timeout: 60,
   },
   http: {
     port: 8000,
-    allow_origin: '*'
+    allow_origin: '*',
+    api: true,
   },
   auth: {
-    api : true,
-    api_user: 'admin',
-    api_pass: 'admin',
+    api: true,
+    api_user: process.env.USERNAME || 'admin',
+    api_pass: process.env.PASSWORD || 'admin',
   },
 };
 
