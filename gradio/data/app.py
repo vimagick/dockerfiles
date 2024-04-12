6b10128a6a0cfb7ad639a7516d531f20b3989b97
kev <vimagick@gmail.com>
2024-04-12 12:05:05 +0800

    update

--- gradio/data/app.py
+++ gradio/data/app.py
@@ -5,5 +5,5 @@ import gradio as gr
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text", allow_flagging="never")
 demo.launch()
