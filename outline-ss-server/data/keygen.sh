17695aed57df390f08ce0039cd3e57a54c5fbd12
kev <vimagick@gmail.com>
2024-09-14 16:00:41 +0800

    update outline-ss-server

--- outline-ss-server/data/keygen.sh
+++ outline-ss-server/data/keygen.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-# pwgen 12 1
+# pwgen -1 12 1
 
 NAME=${1:?name is empty}
 REMARK=${2:-EasyPi}
-SERVER="127.0.0.1:9443"
+SERVER=127.0.0.1
+PORT=8388
 CIPHER="chacha20-ietf-poly1305"
-SECRET=$(yq -o json config.yml | jq -e -r --arg name "$NAME" '.keys[]|select(.id==$name).secret')
+SECRET=$(
+  yq -o json config.yml |
+    jq -e -r --arg name "$NAME" --arg addr "[::]:$PORT" '.services[]|select(.listeners[0].address==$addr).keys[]|select(.id==$name).secret'
+)
 DIGEST=$(echo -n "$CIPHER:$SECRET" | base64 | sed 's/=*$//')
 
-echo "ss://$DIGEST@$SERVER/?outline=1#$REMARK"
+echo "ss://$DIGEST@$SERVER:$PORT/?outline=1#$REMARK"
